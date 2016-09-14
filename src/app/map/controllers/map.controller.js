0a04d3e9c94cfef2b249593de9ca86f70c970213
Mykhas Kobernyk <m.kobernyk@gmail.com>
2016-09-14 09:22:30 +0300

    Coordinates JSON added

--- src/app/map/controllers/map.controller.js
+++ src/app/map/controllers/map.controller.js
@@ -2,7 +2,7 @@ import leaflet from 'leaflet';
 
 class MapController {
     constructor() {
-        this.map = L.map('map').setView([51.505, -0.09], 13);
+        this.map = L.map('map').setView([50.45, 30.52], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
--- src/app/search/controllers/search.controller.js
+++ src/app/search/controllers/search.controller.js
@@ -1,6 +1,6 @@
 class SearchController {
     constructor() {
-
+        
     }
 }
 
